aca657ea11bed5c87d2597f0dd91c3d01bac6f20
heekang2271 <hk.shin2271@gmail.com>
2023-11-09 20:23:03 +0900

    중력 및 꼬리 효과 구현

--- canvas/2-fireflower/src/js/App.js
+++ canvas/2-fireflower/src/js/App.js
@@ -13,27 +13,30 @@ class App extends Canvas {
     this.resize();
 
     this.particles = [];
-    // const x = randomNumBetween(0, this.canvasWidth);
-    // const y = randomNumBetween(0, this.canvasHeight);
     const x = this.canvasWidth / 2;
     const y = this.canvasHeight / 2;
     for (let i = 0; i < this.totalParticles; i++) {
-      const r = randomNumBetween(1, 5);
+      const r = randomNumBetween(1, 20);
       const angle = (Math.PI / 180) * randomNumBetween(0, 360);
       const vx = r * Math.cos(angle);
       const vy = r * Math.sin(angle);
-      this.particles.push(new Particle(x, y, vx, vy));
+      const opacity = randomNumBetween(0.6, 0.9);
+      this.particles.push(new Particle(x, y, vx, vy, opacity));
     }
   }
 
   render() {
     const draw = () => {
-      this.ctx.fillStyle = '#000000';
+      this.ctx.fillStyle = '#000000' + 40;
       this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
 
-      this.particles.forEach((particle) => {
-        particle.update();
+      this.particles.forEach((particle, i) => {
         particle.draw();
+        particle.update();
+
+        if (particle.opacity < 0.1) {
+          this.particles.splice(i, 1);
+        }
       });
     };
 
--- canvas/2-fireflower/src/js/Particle.js
+++ canvas/2-fireflower/src/js/Particle.js
@@ -1,24 +1,34 @@
 import CanvasOption from './CanvasOption.js';
 
 export default class Particle extends CanvasOption {
-  constructor(x, y, vx, vy) {
+  constructor(x, y, vx, vy, opacity) {
     super();
 
     this.x = x;
     this.y = y;
-
     this.vx = vx;
     this.vy = vy;
+    this.opacity = opacity;
+
+    this.friction = 0.93;
+    this.gravity = 0.12;
   }
 
   update() {
+    this.vy += this.gravity;
+
+    this.vy *= this.friction;
+    this.vx *= this.friction;
+
     this.x += this.vx;
     this.y += this.vy;
+
+    this.opacity -= 0.02;
   }
 
   draw() {
     this.ctx.beginPath();
-    this.ctx.fillStyle = '#ffffff';
+    this.ctx.fillStyle = `rgba(255, 255, 255, ${this.opacity})`;
     this.ctx.arc(this.x, this.y, 1, 0, Math.PI * 2);
     this.ctx.fill();
     this.ctx.closePath();
